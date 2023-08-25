a559d316d86a8ab672cd5e1b76c1c3f31d3faf41
apanda1001 <apanda20@iitk.ac.in>
2023-08-25 19:59:23 +0530

    whoopsie, made a mistake. corrected it ~pandachan

--- assn3/submit.py
+++ assn3/submit.py
@@ -1,109 +1,33 @@
 import numpy as np
-from sklearn.svm import LinearSVC
+import pickle as pkl
+from sklearn.ensemble import RandomForestRegressor
+# Define your prediction method here
+# We are using the Random Forest mechanism for regression. Two models are being used for O3 and NO2 predictions, and the time is calculated according to the heuristic described in the report.
+# df is a dataframe containing timestamps, weather data and potentials
+def my_predict( df ):
+    rand_for_ozone = pkl.load(open("rand_for_ozone_time.pkl", 'rb'))
+    rand_for_no2 = pkl.load(open("rand_for_no2_time.pkl", 'rb'))
+    x1 = df["no2op1"].to_numpy()
+    x2 = df["no2op2"].to_numpy()
+    x3 = df["o3op1"].to_numpy()
+    x4 = df["o3op2"].to_numpy()
+    x5 = df["temp"].to_numpy()
+    x6 = df["humidity"].to_numpy()
+    time = df["Time"].to_numpy()
+    in_the_moonlight = []
+    for i in range(0, time.shape[0]):
+        t = time[i][11:]
+        if t >= "19:45:00" or t <= "06:55:00": # average sunset and sunrise time
+            in_the_moonlight.append(0.0)
+        else:
+            in_the_moonlight.append(1.0)
 
-# You are allowed to import any submodules of sklearn as well e.g. sklearn.svm etc
-# You are not allowed to use other libraries such as scipy, keras, tensorflow etc
-
-# SUBMIT YOUR CODE AS A SINGLE PYTHON (.PY) FILE INSIDE A ZIP ARCHIVE
-# THE NAME OF THE PYTHON FILE MUST BE submit.py
-# DO NOT INCLUDE OTHER PACKAGES LIKE SCIPY, KERAS ETC IN YOUR CODE
-# THE USE OF ANY MACHINE LEARNING LIBRARIES OTHER THAN SKLEARN WILL RESULT IN A STRAIGHT ZERO
-
-# DO NOT CHANGE THE NAME OF THE METHODS my_fit, my_predict etc BELOW
-# THESE WILL BE INVOKED BY THE EVALUATION SCRIPT. CHANGING THESE NAMES WILL CAUSE EVALUATION FAILURE
-
-# You may define any new functions, variables, classes here
-# For example, functions to calculate next coordinate or step length
-
-def createFeatures( X ):
-	# Create features for the training data
-    return X
-
-################################
-# Non Editable Region Starting #
-################################
-def my_fit( Z_train ):
-################################
-#  Non Editable Region Ending  #
-################################
-
-	# Use this method to train your model using training CRPs
-	# The first 64 columns contain the config bits
-	# The next 4 columns contain the select bits for the first mux
-	# The next 4 columns contain the select bits for the second mux
-	# The first 64 + 4 + 4 = 72 columns constitute the challenge
-	# The last column contains the response
-
-	R = 64
-	S1 = 4
-	S2 = 4
-
-	# Split the training data into challenges, select bits and responses
-	X_trn = Z_train[:, :R]
-	p = Z_train[:,R:R+S1]
-	q = Z_train[:,R+S1:R+S1+S2]
-	Y_trn = Z_train[:,-1]
-
-	train_dict = {}	# Dictionary to store the training data for each xorro
-
-	num_xorro = 2**S1	# S1==S2 so num_xorro = 2**S2
-
-	for a in range(16):
-		for b in range(16):
-			train_dict[(a,b)] = []
-
-	for i in range(Z_train.shape[0]):
-		p_dec = 8*int(p[i,0]) + 4*int(p[i,1]) + 2*int(p[i,2]) + int(p[i,3])
-		q_dec = 8*int(q[i,0]) + 4*int(q[i,1]) + 2*int(q[i,2]) + int(q[i,3])
-
-		train_dict[(p_dec,q_dec)].append(i)
-
-	# Train a model for each xorro
-	models = {}
-	for a in range(num_xorro):
-		for b in range(num_xorro):
-			if a==b:
-				continue
-			if a>b:
-				model = LinearSVC(loss='hinge')
-				indices = train_dict[(a,b)]
-				indices_neg = train_dict[(b,a)]
-				c_data = np.append(X_trn[indices], X_trn[indices_neg], axis=0)
-				r_data = np.append(Y_trn[indices], 1-Y_trn[indices_neg], axis=0)
-				model.fit(createFeatures(c_data), r_data)
-				models[(a,b)] = model
-
-	return models					# Return the trained model
-
-
-################################
-# Non Editable Region Starting #
-################################
-def my_predict( X_tst, models):
-################################
-#  Non Editable Region Ending  #
-################################
-	# Use this method to make predictions on test challenges
-	R = 64
-	S1 = 4
-	S2 = 4
-
-	# Split the test data into challenges, select bits and responses
-	X_test = X_tst[:, :R]
-	p = X_tst[:,R:R+S1]
-	q = X_tst[:,R+S1:R+S1+S2]
-
-	# Predict the response for each challenge
-	Y_pred = np.zeros(len(X_tst))
-	for i in range(X_tst.shape[0]):
-		p_dec = 8*int(p[i,0]) + 4*int(p[i,1]) + 2*int(p[i,2]) + int(p[i,3])
-		q_dec = 8*int(q[i,0]) + 4*int(q[i,1]) + 2*int(q[i,2]) + int(q[i,3])
-
-		if p_dec>q_dec:
-			model = models[(p_dec,q_dec)]
-			Y_pred[i] = model.predict(createFeatures(X_test[i].reshape(1,-1)))
-		else:
-			model = models[(q_dec, p_dec)]
-			Y_pred[i] = 1 - model.predict(createFeatures(X_test[i].reshape(1,-1)))
-
-	return Y_pred
+    x7 = np.array(in_the_moonlight) # for ozone
+    X_1 = []
+    for i in range(0, x1.shape[0]):
+        x = np.array([x1[i], x2[i], x3[i], x4[i], x5[i], x6[i], x7[i]])
+        X_1.append(x)
+    X = np.array(X_1)
+    pred_o3 = rand_for_ozone.predict(X)
+    pred_no2 = rand_for_no2.predict(X)
+    return ( pred_o3, pred_no2 )
